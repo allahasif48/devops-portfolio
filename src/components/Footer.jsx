ce952c8f7d2043db0e97a90fe6ec4d001ec1f469
ASIFALI SHAIK <allahasif48@gmail.com>
2025-06-05 20:41:54 -0400

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-gray-400 text-center py-4 mt-16">
+      <p>© {new Date().getFullYear()} Asifali Shaik. All rights reserved.</p>
+    </footer>
+  );
+}
